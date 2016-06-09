a9972664faf69f5e0dbc1f34cddefd438a1ed1bc
LiuchangDuan <870121425@qq.com>
2016-06-09 15:04:52 +0800

    获取并解析Json数据 实现ListView图文混排

---
+++ newsdemo/src/main/java/com/example/newsdemo/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.newsdemo;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    private static String URL = "http://www.imooc.com/api/teacher?type=4&num=30";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mListView = (ListView) findViewById(R.id.lv_main);
+        new NewsAsyncTask().execute(URL);
+    }
+
+    /**
+     * 将url对应的JSON格式数据转化为我们所封装的NewsBean
+     * @param url
+     * @return
+     */
+    private List<NewsBean> getJsonData(String url) {
+        List<NewsBean> newsBeanList = new ArrayList<>();
+        try {
+            //此句功能与url.openConnection().getInputStream()相同
+            //可根据URL直接联网获取网络数据
+            //返回值类型为InputStream
+            String jsonString = readStream(new java.net.URL(url).openStream());
+            JSONObject jsonObject;
+            NewsBean newsBean;
+            try {
+                jsonObject = new JSONObject(jsonString);
+                JSONArray jsonArray = jsonObject.getJSONArray("data");
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    jsonObject = jsonArray.getJSONObject(i);
+                    newsBean = new NewsBean();
+                    newsBean.newsIconUrl = jsonObject.getString("picSmall");
+                    newsBean.newsTitle = jsonObject.getString("name");
+                    newsBean.newsContent = jsonObject.getString("description");
+                    newsBeanList.add(newsBean);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+//            Log.d("Main", jsonString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return newsBeanList;
+
+    }
+
+    /**
+     * 通过is解析网页返回的数据
+     * @param is
+     * @return
+     */
+    private String readStream(InputStream is) {
+        InputStreamReader isr;
+        String result = "";
+        try {
+            String line = "";
+            isr = new InputStreamReader(is, "utf-8"); // 字节流转化为字符流
+            BufferedReader br = new BufferedReader(isr);
+            while((line = br.readLine()) != null) {
+                result += line;
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    /**
+     * 实现网络的异步访问
+     */
+    class NewsAsyncTask extends AsyncTask<String, Void, List<NewsBean>> {
+
+        @Override
+        protected List<NewsBean> doInBackground(String... params) {
+            return getJsonData(params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(List<NewsBean> newsBeans) {
+            super.onPostExecute(newsBeans);
+            NewsAdapter adapter = new NewsAdapter(MainActivity.this, newsBeans);
+            mListView.setAdapter(adapter);
+        }
+    }
+
+}
---
+++ newsdemo/src/main/java/com/example/newsdemo/NewsBean.java
@@ -0,0 +1,12 @@
+package com.example.newsdemo;
+
+/**
+ * Created by Administrator on 2016/6/9.
+ */
+public class NewsBean {
+
+    public String newsIconUrl;
+    public String newsTitle;
+    public String newsContent;
+
+}
